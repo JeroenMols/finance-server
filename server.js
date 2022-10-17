3fd67119f674ce6a7431526c522c2fb00e11f56f
Jeroen Mols <jeroen@jeroenmols.com>
2022-10-17 13:02:00 +0200

    bare bones server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+const https = require('https');
+
+
+app.get('/stocks/:ticker', (req, res) => {
+  let ticker = req.params.ticker
+
+  https.get('https://query1.finance.yahoo.com/v7/finance/quote?symbols=' + ticker, (resp) => {
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+    res.send(data)
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
