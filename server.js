3c4e8c9f38f4b4037c02e505b789ce8a9b826666
Jeroen Mols <jeroen@jeroenmols.com>
2022-10-21 21:36:39 +0200

    Send optimized data models to client

--- server.js
+++ server.js
@@ -20,32 +20,53 @@ app.use(cors(corsOptions))
 
 const https = require('https');
 
-
-app.get('/stocks/:ticker', (req, res) => {
+app.get('/stocks/:ticker/:amount', (req, res) => {
   let ticker = req.params.ticker
-
+  let amount = parseInt(req.params.amount)
+  
   https.get('https://query1.finance.yahoo.com/v7/finance/quote?symbols=' + ticker, (resp) => {
-  let data = '';
+    let data = '';
 
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      const resultJson = JSON.parse(data)
+      console.log(resultJson);
+      let stockData = toStockData(resultJson, amount)
+      console.log(stockData)
+      res.send(stockData)
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
   });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
-    res.send(data)
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-
-
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function toStockData(quoteData, shares) {
+  let result = quoteData.quoteResponse.result[0];
+  let price = parseFloat(result.regularMarketPrice)
+  let stockData = new StockData(result.longName, 
+    result.symbol, 
+    parseFloat(price.toFixed(2)), 
+    shares,
+    parseFloat((price * shares).toFixed(2)));
+  return stockData;
+}
+
+class StockData {
+  constructor(name, ticker, price, shares, totalValue) {
+    this.name = name
+    this.ticker = ticker
+    this.price = price
+    this.shares = shares
+    this.totalValue = totalValue
+  }
+}
