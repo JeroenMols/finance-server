81f0d4c815506cc884949a441e9f2416083fff30
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 11:00:59 +0100

    Add comment to track SSL production work

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const PORT = process.env.PORT || 4000;
 export const CORS_ORIGIN = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 export const DATABASE_URL = process.env.DATABASE_URL || createLocalDbUrl();
+// TODO make SSL on production work
 export const DATABASE_SSL =
   process.env.NODE_ENV == 'production'
     ? {
