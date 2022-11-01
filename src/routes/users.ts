97cec088d416877ae01ebaf6e6973c6a937a0cb6
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 10:04:29 +0100

    Simple POC to communicate with database

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from 'express';
 import crypto from 'crypto';
+import { QueryResult } from 'pg';
+
+const db = require('../db');
 
 const router = express.Router();
 
 // TODO should probably be post
 router.get('/create', (req: Request, res: Response): void => {
+  db.query('SELECT * FROM account').then((it: QueryResult) => {
+    console.log(it.rows);
+  });
   res.send(crypto.randomUUID());
 });
 
