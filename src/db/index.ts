1bd79a3487b8e861b7672d49444e91e173a46383
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 11:51:06 +0100

    Add database logging

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'pg';
+import { Pool, QueryResult } from 'pg';
 import { DATABASE_SSL, DATABASE_URL } from '../config';
 
 const pool = new Pool({
@@ -7,5 +7,11 @@ const pool = new Pool({
 });
 
 module.exports = {
-  query: (text: string, params: string[]) => pool.query(text, params),
+  async query(text: string, params: string[]): Promise<QueryResult> {
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log('executed query', { text, duration, rows: res.rowCount });
+    return res;
+  },
 };
