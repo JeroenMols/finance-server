78e71f8b56604001a9a5fd2fd7db45415e9dc090
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 11:22:48 +0100

    Use async routes

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,26 +1,24 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import crypto from 'crypto';
-import { QueryResult } from 'pg';
 
 const db = require('../db');
 
-const router = express.Router();
+const router = require('express-promise-router')();
 
 // TODO should probably be post
-router.get('/create', (req: Request, res: Response): void => {
+router.get('/create', async (req: Request, res: Response): Promise<void> => {
   const userId = crypto.randomUUID();
-  db.query('INSERT into account(uuid) values ($1)', [userId]).then((it: QueryResult) => {
-    console.log(it.rows);
-    res.send(userId);
-  });
+  const { rows } = db.query('INSERT into account(uuid) values ($1)', [userId]);
+
+  console.log(rows);
+  res.send(userId);
 });
 
 // TODO temporary endpoint needs to be removed
-router.get('/debug', (req: Request, res: Response): void => {
-  db.query('SELECT * FROM account').then((it: QueryResult) => {
-    console.log(it.rows);
-    res.send(it.rows);
-  });
+router.get('/debug', async (req: Request, res: Response): Promise<void> => {
+  const { rows } = db.query('SELECT * FROM account');
+  console.log(rows);
+  res.send(rows);
 });
 
 export default router;
