33a57c251d743b7f77d0a4566463bf4126787810
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 10:09:38 +0100

    Basic create user and get users functionality

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -8,10 +8,19 @@ const router = express.Router();
 
 // TODO should probably be post
 router.get('/create', (req: Request, res: Response): void => {
+  const userId = crypto.randomUUID();
+  db.query('INSERT into account(uuid) values ($1)', [userId]).then((it: QueryResult) => {
+    console.log(it.rows);
+    res.send(userId);
+  });
+});
+
+// TODO temporary endpoint needs to be removed
+router.get('/get', (req: Request, res: Response): void => {
   db.query('SELECT * FROM account').then((it: QueryResult) => {
     console.log(it.rows);
+    res.send(it.rows);
   });
-  res.send(crypto.randomUUID());
 });
 
 export default router;
