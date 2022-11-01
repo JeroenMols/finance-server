11f7b447d2f3e24c7295028cc705c30a35769586
Jeroen Mols <jeroen@jeroenmols.com>
2022-11-01 14:32:15 +0100

    Add login

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Postgress
 
 `create table account (id serial primary key, uuid varchar(40) unique not null, created_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP);`
+
+`create table login (id serial primary key, account_id integer not null, access_token varchar(50) unique not null, created_at TIMESTAMP WITH TIME ZONE NOT NULL default CURRENT_TIMESTAMP, expired_at TIMESTAMP WITH TIME ZONE NOT NULL, CONSTRAINT fk_account FOREIGN KEY(account_id) references account(id) on delete cascade)`
--- src/routes/account.ts
+++ src/routes/account.ts
@@ -5,7 +5,7 @@ const db = require('../db');
 
 const router = require('express-promise-router')();
 
-router.get('/create', async (req: Request, res: Response): Promise<void> => {
+router.post('/create', async (req: Request<unused, unused, AccountCreate>, res: Response): Promise<void> => {
   // TODO setup a proper application secret here
   if (req.body.application_secret !== 'mysecret') {
     console.log('unautorized - wrong secret');
@@ -21,6 +21,37 @@ router.get('/create', async (req: Request, res: Response): Promise<void> => {
   res.send(userId);
 });
 
+router.post('/login', async (req: Request<unused, unused, AccountLogin>, res: Response): Promise<void> => {
+  const accountUuid = req.body.account_id;
+  if (!accountUuid) {
+    console.log('bad request - missing account_id');
+    res.status(404);
+    res.send({ error: 'please provide a valid account id' });
+    return;
+  }
+  console.log(accountUuid);
+
+  const accountResult = await db.query('SELECT id, uuid from account where uuid = ($1)', [accountUuid]);
+  if (accountResult.rowCount < 1) {
+    res.status(401);
+    res.send({ error: 'user does not exist' });
+    return;
+  }
+
+  const accountId = (accountResult.rows[0] as Account).id;
+  const accessToken = `access-token-${crypto.randomUUID()}`;
+  const expirationDate = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000);
+
+  const { rows } = await db.query('INSERT into login(account_id, access_token, expired_at) values ($1, $2, $3)', [
+    accountId,
+    accessToken,
+    expirationDate,
+  ]);
+
+  console.log(rows);
+  res.send(accessToken);
+});
+
 // TODO temporary endpoint needs to be removed
 router.get('/debug', async (req: Request, res: Response): Promise<void> => {
   const { rows } = await db.query('SELECT * FROM account');
@@ -31,3 +62,12 @@ router.get('/debug', async (req: Request, res: Response): Promise<void> => {
 export default router;
 
 type AccountCreate = { application_secret: string };
+type AccountLogin = { account_id: string };
+
+type Account = {
+  id: number;
+  uuid: string;
+  created_at: string;
+};
+
+type unused = Record<string, unknown>;
