c9d50a64fbea4fa88e182f4b7ecd1f913d9749e7
Jeroen Mols <jeroen@jeroenmols.com>
2023-01-01 15:15:02 +0100

    Add script to initialize database

---
+++ init-db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE account (
+  id serial PRIMARY KEY,
+  UUID varchar(40) UNIQUE NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE login (
+  id serial PRIMARY KEY,
+  account_id integer NOT NULL,
+  access_token varchar(50) UNIQUE NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  expired_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  CONSTRAINT fk_account FOREIGN KEY(account_id) REFERENCES account(id) ON DELETE CASCADE
+);
+CREATE TABLE holding (
+  id serial PRIMARY KEY,
+  account_id integer NOT NULL,
+  ticker varchar(10) NOT NULL,
+  shares integer NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT fk_account FOREIGN KEY(account_id) REFERENCES account(id) ON DELETE CASCADE
+);
